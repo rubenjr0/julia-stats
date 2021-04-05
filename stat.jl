9f8c5332cea30f2cfe9a21672fa9edc703ce8996
Rubén Jiménez <>
2021-04-05 14:54:01 +0000

    Añadido a help funciones del t2, mejoras en regresion y cambio de nombres de funciones

--- stat.jl
+++ stat.jl
@@ -1,20 +1,22 @@
-# using Plots
-# theme(:wong2)
+using Plots
+theme(:wong2)
 
 function help()
+    printstyled("== Tema 1 ==\n", color=:reverse)
     printstyled("== Medidas estadisticas ==\n", color=:reverse)
     println("= media(x)")
-    println("= σ2(x) Varianza")
-    println("= σ(x) Desviación típica")
+    println("= var(x) Varianza")
+    println("= dt(x) Desviación típica")
     println("= dm(x) Desviación media")
     println("= mediana(x)")
     println("= moda(x)")
-    println("= z(x) Tipificador de x: z(x).(x)\n")
+    println("= z(x) Tipificador de x: z(x).(x)")
+    println("= outliers(x, LB, HB) Outliers con limites inferior LB y superior HB (cuantiles)\n")
     printstyled("== Frecuencias ==\n", color=:reverse)
-    println("= ni(x) Frecuencia absoluta")
-    println("= fi(x) Frecuencia relativa")
-    println("= Ni(x) Frecuencia absoluta acumulada")
-    println("= Fi(x) Frecuencia relativa acumulada\n")
+    println("= ni(x) Frecuencias absolutas")
+    println("= fi(x) Frecuencias relativas")
+    println("= Ni(x) Frecuencias absolutas acumuladas")
+    println("= Fi(x) Frecuencias relativas acumuladas\n")
     printstyled("== Momentos ==\n", color=:reverse)
     println("= mrc(x,r,c) Momento de orden R en el punto C")
     println("= mr(x) Momento ordinario de orden R")
@@ -24,17 +26,28 @@ function help()
     println("= g1(x) Coeficiente de asimetría de Fisher\n")
     printstyled("== Apuntamiento ==\n", color=:reverse)
     println("= g2(x) Coeficiente de aplastamiento de Fisher\n")
+    printstyled("== Tema 2 ==\n", color=:reverse)
+    printstyled("== Regresion ==\n", color=:reverse)
+    println("= reg_lin(x, y) Regresion lineal ax+b")
+    println("= reg_exp(x, y) Regresion exponencial a*b^x")
+    printstyled("== Predictores ==\n", color=:reverse)
+    println("= pred_lin(x, y) Construye un predictor lineal")
+    println("= pred_exp(x, y) Construye un predictor exponencial")
+    printstyled("== Error ==\n", color=:reverse)
+    println("= err(x, y, p) Calcula el error del predictor")
+    printstyled("== Visualización ==\n", color=:reverse)
+    println("= plot_reg(x, y, p) Visualiza la regresion con el predictor p\n")
 end
 
 # Base
 media(x) = sum(x) / length(x)
 cov(x, y) = sum(x .* y) / length(x) - media(x) * media(y)
-σ2(x) = cov(x, x)
-σ(x) = sqrt(σ2(x))
+var(x) = cov(x, x)
+dt(x) = sqrt(var(x))
 dm(x) = media(x .- media(x))
 mediana(x) = quantile(x, 0.5)
 moda(x) = unique(x)[findmax(ni(x))[2]]
-z(x) = k -> (k - media(x)) / σ(x)
+z(x) = k -> (k - media(x)) / dt(x)
 
 # Freqs
 ni(x) = [count(==(xi), x) for xi in unique(x)]
@@ -55,16 +68,16 @@ function ni(x)
 end =#
 
 # Momentos
-mrc(x, c, r) = sum(((unique(x) .- c).^r) .* fi(x))
+mrc(x, r, c) = sum(((unique(x) .- c).^r) .* fi(x))
 mr(x, r) = mrc(x, 0, r)
 μr(x, r) = mrc(x, media(x), r)
 
 # Asimetria
 ap(x) = (media(x) - moda(x)) / σ(x)
-g1(x) = μr(x, 3) / (σ(x)^3)
+g1(x) = μr(x, 3) / (dt(x)^3)
 
 # Apuntamiento
-g2(x) = μr(x, 4) / (σ2(x)^2) - 3
+g2(x) = μr(x, 4) / (var(x)^2) - 3
 
 # Outliers
 function outliers(x, LB, HB)
@@ -94,31 +107,31 @@ end
 
 # Regression
 function reg_lin(x, y) 
-    a = cov(x, y) / σ2(x)
+    a = cov(x, y) / var(x)
     b = media(y) - a * media(x)
     return [a, b]
 end
 
 function reg_exp(x, y)
     logy = log.(y)
-    a = cov(x, logy) / σ2(x)
+    a = cov(x, logy) / var(x)
     b = media(logy) - a * media(x)
     return [exp(a), exp(b)]
 end
 
 # Predictors
-function predictor_lin(x, y)
+function pred_lin(x, y)
     a, b = reg_lin(x, y)
     return x -> a * x + b
 end
 
-function predictor_exp(x, y)
+function pred_exp(x, y)
     a, b = reg_exp(x, y)
     return x -> a^x * b
 end
 
 # Errors
-err(x,y,f) = sum((f.(x) .- y).^2)
+err(x,y,p) = sum((p(x,y).(x) .- y).^2)
 
 # Plots
 function plot_reg(x, y, p)
